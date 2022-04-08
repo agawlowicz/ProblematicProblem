cfda303291540c05e66902bc1a67b03418913765
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-08 10:09:30 -0500

    Random activity generator, debugging

---
+++ ProblematicProblem/BoolValues.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ProblematicProblem
+{
+    public class BoolValues
+    {
+        public BoolValues()
+        {
+        }
+
+        public static bool GetBoolValue(string s)
+        {
+            switch (s.ToLower())
+            {
+                case "yes":
+                case "sure":
+                case "redo": //redo means keep cont true so it runs again
+                    return true;
+                case "no":
+                case "no thanks":
+                case "keep": //keep stops the program so cont must be false
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
